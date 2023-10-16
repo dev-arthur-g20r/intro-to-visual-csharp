2af1f2b46a454b9926f03883fb850504df202723
Arthur Tristan M. Ramos <52824403+dev-arthur-g20r@users.noreply.github.com>
2023-10-16 19:32:37 +0800

    Initial commit

---
+++ ProjectAlpha/ProjectAlpha/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ProjectAlpha
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.firstNumberTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.operatorComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.secondNumberTextBox = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Number:";
+            // 
+            // firstNumberTextbox
+            // 
+            this.firstNumberTextbox.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNumberTextbox.Location = new System.Drawing.Point(161, 13);
+            this.firstNumberTextbox.Name = "firstNumberTextbox";
+            this.firstNumberTextbox.Size = new System.Drawing.Size(627, 31);
+            this.firstNumberTextbox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Operator:";
+            // 
+            // operatorComboBox
+            // 
+            this.operatorComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operatorComboBox.FormattingEnabled = true;
+            this.operatorComboBox.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.operatorComboBox.Location = new System.Drawing.Point(161, 50);
+            this.operatorComboBox.Name = "operatorComboBox";
+            this.operatorComboBox.Size = new System.Drawing.Size(627, 32);
+            this.operatorComboBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "2nd Number:";
+            // 
+            // secondNumberTextBox
+            // 
+            this.secondNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondNumberTextBox.Location = new System.Drawing.Point(161, 95);
+            this.secondNumberTextBox.Name = "secondNumberTextBox";
+            this.secondNumberTextBox.Size = new System.Drawing.Size(627, 29);
+            this.secondNumberTextBox.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(249, 153);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(281, 71);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.secondNumberTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.operatorComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.firstNumberTextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox firstNumberTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox operatorComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox secondNumberTextBox;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ ProjectAlpha/ProjectAlpha/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectAlpha
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private string calculate(
+            double firstNumber,
+            string arithmeticOperator,
+            double secondNumber
+        ) {
+            string resultStatement = "";
+            double result = 0.0;
+
+            switch (arithmeticOperator)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+            }
+
+            resultStatement = firstNumber + " " + arithmeticOperator + " " + secondNumber + " = " + result;
+
+            return resultStatement;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string messageBoxTitle = "Calculator Result";
+            string messageBoxBody = "";
+
+            try {
+                double firstNumber = Convert.ToDouble(firstNumberTextbox.Text);
+                string arithmeticOperator = operatorComboBox.Text;
+                double secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+                string calculationResult = calculate(firstNumber, arithmeticOperator, secondNumber);
+                
+                messageBoxBody = calculationResult;
+            } catch {
+                messageBoxBody = "Invalid operation!";
+            }
+
+            MessageBox.Show(messageBoxBody, messageBoxTitle);
+        }
+    }
+}
